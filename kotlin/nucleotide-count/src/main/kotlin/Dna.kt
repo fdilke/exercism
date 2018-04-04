3e0098409856a3d3a274ddc625da4da809dea427
Felix <knowledge-graph-dev@springernature.com>
2018-04-04 14:28:08 +0100

    completed nucleotide-count exercise

--- kotlin/nucleotide-count/src/main/kotlin/Dna.kt
+++ kotlin/nucleotide-count/src/main/kotlin/Dna.kt
@@ -1,4 +1,16 @@
 class Dna(text: String) {
+  private val nucleotides: List<Char> =
+    "ACGT".toList()
+
+  init {
+    if (!text.toList().minus(nucleotides).isEmpty())
+      throw IllegalArgumentException()
+  }
+
   val nucleotideCounts: Map<Char, Int> =
-      mapOf()
+    nucleotides.map { ch ->
+      Pair(ch, text.toList().count {
+        it == ch
+      })
+    }.toMap()
 }
